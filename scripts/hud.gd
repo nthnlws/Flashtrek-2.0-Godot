9658e9c52a7599686f830c6da2564b7c95470424
nthnlws <88010944+nthnlws@users.noreply.github.com>
2024-07-30 22:27:00 -0500

    First APK and boot bug
    Created first signed APK (no controls).
    Fixed boot bug related to duplicate classes.

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -31,7 +31,7 @@ func init_lives(amount):
 		lives.add_child(ul)
 
 func _process(delta):
-	$Variable.text = "Last Col: " + str(round(player.energy_current))
+	$Variable.text = "Energy: " + str(round(player.energy_current))
 	
 	healthBar.value = player.hp_current
 	healthBar.max_value = player.hp_max
