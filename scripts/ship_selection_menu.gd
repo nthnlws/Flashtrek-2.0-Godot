cd2cb424cb2ead7332178e251bb93a6ac7ffc09c
nthnlws <88010944+nthnlws@users.noreply.github.com>
2024-11-22 15:07:14 -0500

    Ship menu sprites
    Ship selection menu now displays list of available ships per faction, otherwise uses default image.

--- scripts/ship_selection_menu.gd
+++ scripts/ship_selection_menu.gd
@@ -10,20 +10,33 @@ extends Control
 @export var num_rom:int
 @export var num_neut:int
 
-var sprite_coords = {
+
+var fed_ships = {
 	"USS_Cerritos": Rect2(601, 1204, 301, 301),
 	"EntTNG": Rect2(1806, 0, 301, 301),
-	"Discovery": Rect2(1806, 1505, 301, 301),
+	"Discovery": Rect2(1806, 1505, 301, 301)
+	}
+var klin_ships = {
+	"brel": Rect2(301, 1504, 301, 301)
+	}
+	
+var rom_ships = {
+	"USS_Cerritos": Rect2(601, 1204, 301, 301)
+	}
+	
+var neut_ships = {
 	"Monaveen": Rect2(1204, 602, 301, 301),
-	"b'rel": Rect2(301, 1504, 301, 301),
-}
+	"dvor": Rect2(301, 903, 301, 301)
+	}
+
+var ship_types = [fed_ships, klin_ships, rom_ships, neut_ships]
 var frame_textures = [federation_frame, klingon_frame, romulan_frame, neutral_frame]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var width = max(num_fed, num_klin, num_rom, num_neut)*100
 	$VBoxContainer.position.x = (960/2) - (50)
-	print($VBoxContainer.position.x)
+	
 	for i in range(num_fed):
 		create_selection_frames(Utility.FACTION.FEDERATION, i)
 	for i in range(num_klin):
@@ -47,63 +60,51 @@ func create_selection_frames(faction, i):
 
 	# Create the frame
 	var frame = TextureButton.new()
-	frame.texture_normal = get_frame_texture(faction)
 	frame.scale = Vector2(1.5, 1.5)
 
 	# Create the ship image
 	var ship_image = TextureRect.new()
 	var atlas_texture = AtlasTexture.new()
 	atlas_texture.atlas = preload("res://assets/textures/ships/ship_sprites.png")
-	atlas_texture.region = Rect2(1204, 0, 301, 301)  # Define the region in the atlas
+	atlas_texture.region = Rect2(1204, 0, 301, 301)  # Default placeholder ship image
 	ship_image.texture = atlas_texture
 	ship_image.scale = Vector2(0.25, 0.25)
-	ship_image.position = Vector2(11, 11)
-	ship_image.size_flags_horizontal = 4
-	ship_image.size_flags_vertical = 4
-	ship_image.anchor_left = 1
-	ship_image.anchor_right = 0
-	ship_image.anchor_top = 0
-	ship_image.anchor_bottom = 1
+	ship_image.position = Vector2(13, 11)
 
 	# Ensure the ship image is behind the frame
 	ship_image.z_index = -1
 	frame.z_index = 1  # Higher value ensures it appears on top
 
-	# Add the ship image and frame to the container
-
-
 	# Add the container to the appropriate faction grid
 	match faction:
 		Utility.FACTION.FEDERATION:
 			%FederationGrid.add_child(container)
+			frame.texture_normal = federation_frame
 			frame.name = "Fed" + str(i + 1)
 			container.name = "Fed_Container_" + str(i + 1)
-			if i == 0: atlas_texture.region = sprite_coords["USS_Cerritos"]
-			if i == 1: atlas_texture.region = sprite_coords["EntTNG"]
+			if i < ship_types[Utility.FACTION.FEDERATION].size():
+				atlas_texture.region = ship_types[Utility.FACTION.FEDERATION].values()[i]
 		Utility.FACTION.KLINGON:
 			%KlingonGrid.add_child(container)
+			frame.texture_normal = klingon_frame
 			frame.name = "Klin" + str(i + 1)
 			container.name = "Klin_Container_" + str(i + 1)
-			if i == 0: atlas_texture.region = sprite_coords["b'rel"]
+			if i < ship_types[Utility.FACTION.KLINGON].size():
+				atlas_texture.region = ship_types[Utility.FACTION.KLINGON].values()[i]
 		Utility.FACTION.ROMULAN:
 			%RomulanGrid.add_child(container)
+			frame.texture_normal = romulan_frame
 			frame.name = "Rom" + str(i + 1)
 			container.name = "Rom_Container_" + str(i + 1)
+			if i < ship_types[Utility.FACTION.ROMULAN].size():
+				atlas_texture.region = ship_types[Utility.FACTION.ROMULAN].values()[i]
 		Utility.FACTION.NEUTRAL:
 			%NeutralGrid.add_child(container)
+			frame.texture_normal = neutral_frame
 			frame.name = "Neut" + str(i + 1)
 			container.name = "Neut_Container_" + str(i + 1)
+			if i < ship_types[Utility.FACTION.NEUTRAL].size():
+				atlas_texture.region = ship_types[Utility.FACTION.NEUTRAL].values()[i]
 		
 	container.add_child(ship_image)
 	container.add_child(frame)
-
-func get_frame_texture(faction):
-	match faction:
-		Utility.FACTION.FEDERATION:
-			return federation_frame
-		Utility.FACTION.KLINGON:
-			return klingon_frame
-		Utility.FACTION.ROMULAN:
-			return romulan_frame
-		Utility.FACTION.NEUTRAL:
-			return neutral_frame
